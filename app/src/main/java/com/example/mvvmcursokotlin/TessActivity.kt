f5ba64ac4b119d903293165852c6269e0b912f02
Edwin Flores <isctovar15@gmail.com>
2021-03-18 18:18:19 -0600

    Add ActivityTess V3

---
+++ app/src/main/java/com/example/mvvmcursokotlin/TessActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mvvmcursokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tess)
+    }
+}
